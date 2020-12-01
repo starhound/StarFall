78c5a521c7eb8f72bcd5b72d0b708f987dd5c552
Starhound <wesleyreid0@gmail.com>
2020-11-30 23:25:35 -0500

    some work

--- Build/Program.cs
+++ Build/Program.cs
@@ -4,48 +4,105 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Build
 {
     class Program
     {
+        static bool exitSystem = false;
+
+        #region Trap application termination
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+        private delegate bool EventHandler(CtrlType sig);
+        static EventHandler _handler;
+        static List<string> MatrixPaths = new List<string>();
+
+        enum CtrlType
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+        private static bool Handler(CtrlType sig)
+        {
+            Console.WriteLine("Exiting system due to external CTRL-C, or process kill, or shutdown");
+
+            
+
+            //allow main to run off
+            exitSystem = true;
+
+            //shutdown right away so there are no lingering threads
+            Environment.Exit(-1);
+
+            return true;
+        }
+        #endregion
+
         [DllImport("user32.dll", SetLastError = true)]
         internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
 
-        void GenerateMatrixBinary(int screenCount)
+        static void GenerateMatrixBinary(int screenCount)
         {
-            File.Copy("Matrix.exe", Environment.CurrentDirectory + "/Matrix" + screenCount.ToString() + ".exe");
-            Console.WriteLine("Generated: " + Environment.CurrentDirectory + "/Matrix" + screenCount.ToString() + ".exe");
+            string app = "Matrix" + screenCount.ToString() + ".exe";
+            if (File.Exists(Environment.CurrentDirectory + "\\" + app))
+                return;
+            File.Copy("Matrix.exe", Environment.CurrentDirectory + "\\Matrix" + screenCount.ToString() + ".exe");
+            Console.WriteLine("Generated: " + Environment.CurrentDirectory + "\\Matrix" + screenCount.ToString() + ".exe");
         }
 
-        Process GenerateMatrixProcess(int screenCount)
+        static Process GenerateMatrixProcess(int screenCount)
         {
             GenerateMatrixBinary(screenCount);
-            Process matrix = new Process();
+            var matrix = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "Matrix" + screenCount.ToString() + ".exe",
+                    WindowStyle = ProcessWindowStyle.Normal
+                }
+            };
             return matrix;
         }
 
         static void Main(string[] args)
         {
+            _handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(_handler, true);
+
             List<Process> matrixProcessList = new List<Process>();
             var allScreens = Screen.AllScreens.ToList();
-            var matrix = new Process
-            {
-                StartInfo =
-                {
-                    FileName = "Matrix.exe",
-                    WindowStyle = ProcessWindowStyle.Normal  
-                }
-            };
-
+            
             int screenCount = 0;
 
             foreach(var screen in allScreens)
             {
-                Console.WriteLine(screen.DeviceName);
+                screenCount++;
+                Console.WriteLine("Screen detected: " + screen.DeviceName);
+                Process matrix = GenerateMatrixProcess(screenCount);
+                matrix.Start();
+                MoveWindow(matrix.MainWindowHandle, screen.WorkingArea.Right, screen.WorkingArea.Top, screen.WorkingArea.Width, screen.WorkingArea.Height, false);
             }
 
+            while(Console.ReadKey().Equals('a'))
+            {
+                foreach (var process in Process.GetProcessesByName("Matrix"))
+                {
+                    process.Kill();
+                }
+
+                foreach (string path in MatrixPaths)
+                {
+                    Console.WriteLine("Deleting " + path);
+                    File.Delete(path);
+                }
+            }
             Console.ReadLine();
         }
     }
